5df9a0f591e821acb39ac08d3d63a9406cfa223a
YahavMizrahi <yahav283@gmail.com>
2021-12-24 00:49:29 +0200

    require

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 // import express from "express";
 // import { notifications } from "./src/routes/notifications.js";
 // import { error } from "./src/routes/error.js";
-const notifications = require('./src/routes/notifications.js')
-const error = require('./src/routes/error.js')
-const express = require('express')
+const { notifications } = require("./src/routes/notifications.js");
+const error = require("./src/routes/error.js");
+const express = require("express");
 // import { dbURI } from "./src/services/mongodb.js";
 // import mongoose from "mongoose";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-
+app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-  notifications()
-  error()
-  next();
+  // notifications()
+  // error()
+  next(notifications, error);
 });
 // app.use([notifications, error]);
 
