fd1689289d8f0836a3029d1c5bfb31ccbe60f83b
YahavMizrahi <yahav283@gmail.com>
2021-12-23 09:37:52 +0200

    !

--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -2,11 +2,12 @@ import express from "express";
 import {
   pullRequest,
   getPullRequestData,
+  printData,
 } from "../controllers/notifications/pull-request.js";
 
 export const notifications = express
   .Router()
   .use("/notifications", [
     express.Router().post("/pull-request", getPullRequestData),
-    express.Router().get("/", pullRequest),
+    express.Router().get("/", printData),
   ]);
