c1d2fce82732dce9ecf4ef602ed68d7a18a0da4f
YahavMizrahi <yahav283@gmail.com>
2021-12-23 11:31:42 +0200

    !

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import express from "express";
-import {notifications} from "./src/routes/notifications.js";
-import {error} from "./src/routes/error.js";
+import { notifications } from "./src/routes/notifications.js";
+import { error } from "./src/routes/error.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use([
-    notifications,
-    error
-])
+app.use(express.json);
+
+app.use([notifications, error]);
 
 app.listen(PORT, () => {
-    console.log(
-        "+++++++++++++++_SERVER_RUN_IN_PORT:_" + PORT + "+++++++++++++++"
-    );
+  console.log(
+    "+++++++++++++++_SERVER_RUN_IN_PORT:_" + PORT + "+++++++++++++++"
+  );
 });
