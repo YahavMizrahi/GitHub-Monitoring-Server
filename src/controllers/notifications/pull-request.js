5613843f5ec06cb09aa0f5e5026bb6d338c384c5
YahavMizrahi <yahav283@gmail.com>
2021-12-25 00:54:59 +0200

    bonus

--- src/controllers/notifications/pull-request.js
+++ src/controllers/notifications/pull-request.js
@@ -8,35 +8,46 @@ const {
 const axios = require("axios");
 
 const addPullReqToDB = async (pullReq) => {
-  try {
-    await set(
-      ref(
-        database,
-        `repo/${pullReq.repository.id}/pull-requests/${pullReq.pull_request.id}`
-      ),
-      {
-        pull_id: pullReq["pull_request"]["id"],
-        number: pullReq["number"],
-        title: pullReq["pull_request"]["title"],
-        body: pullReq["pull_request"]["body"],
-        action: pullReq["action"],
-        pull_html_url: pullReq["pull_request"]["html_url"],
-        user_name: pullReq["pull_request"]["user"]["login"],
-        user_id: pullReq["pull_request"]["user"]["id"],
-        created: pullReq["pull_request"]["created_at"],
-        closed: pullReq["pull_request"]["closed_at"],
-        avatarUserUrl: pullReq["pull_request"]["user"]["avatar_url"],
-        user_html_url: pullReq["pull_request"]["user"]["html_url"],
-        repo_html_url: pullReq["repository"]["html_url"],
-        repoName: pullReq["repository"]["name"],
-        repo_id: pullReq["repository"]["id"],
-      }
-    );
-    return true;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
+const getImgPull = async (url) => {
+  return await // screenshot_api.
+  axios
+    .get(
+      `https://shot.screenshotapi.net/screenshot?token=6NHFM97-V4NM40T-Q4ANTBQ-S0SP255=${url}`
+    )
+    .then(async (res) => {
+      return await res.data.screenshot;
+    });
+};
+try {
+  await set(
+    ref(
+      database,
+      `repo/${pullReq.repository.id}/pull-requests/${pullReq.pull_request.id}`
+    ),
+    {
+      pull_id: pullReq["pull_request"]["id"],
+      number: pullReq["number"],
+      title: pullReq["pull_request"]["title"],
+      body: pullReq["pull_request"]["body"],
+      action: pullReq["action"],
+      pull_html_url: pullReq["pull_request"]["html_url"],
+      user_name: pullReq["pull_request"]["user"]["login"],
+      user_id: pullReq["pull_request"]["user"]["id"],
+      created: pullReq["pull_request"]["created_at"],
+      closed: pullReq["pull_request"]["closed_at"],
+      avatarUserUrl: pullReq["pull_request"]["user"]["avatar_url"],
+      user_html_url: pullReq["pull_request"]["user"]["html_url"],
+      repo_html_url: pullReq["repository"]["html_url"],
+      repoName: pullReq["repository"]["name"],
+      repo_id: pullReq["repository"]["id"],
+      img_pull_url: getImgPull([pullReq["pull_request"]["html_url"]]),
+    }
+  );
+  return true;
+} catch (e) {
+  console.log(e);
+  return null;
+}
 };
 
 const getPullReqFromDB = (req, res, next) => {
