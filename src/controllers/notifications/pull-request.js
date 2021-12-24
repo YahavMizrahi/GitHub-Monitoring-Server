aa9e12e3438de44e8023d31dcd8c7bc29038029e
YahavMizrahi <yahav283@gmail.com>
2021-12-24 09:55:25 +0200

    firebase

--- src/controllers/notifications/pull-request.js
+++ src/controllers/notifications/pull-request.js
@@ -13,23 +13,23 @@ const printData = async (req, res) => {
 };
 
 const addPullReqToDB = async (pullReq) => {
-  const pullData = {
-    pull_id: pullReq["pull_request"]["id"],
-    number: pullReq["number"],
-    title: pullReq["pull_request"]["title"],
-    body: pullReq["pull_request"]["body"],
-    action: pullReq["action"],
-    pull_html_url: pullReq["pull_request"]["html_url"],
-    user_name: pullReq["user"]["login"],
-    user_id: pullReq["user.id"],
-    created: pullReq["created_at"],
-    closed: pullReq["closed_at"],
-    avatarUserUrl: pullReq["user"]["avatar_url"],
-    user_html_url: pullReq["user.url"],
-    repo_html_url: pullReq["repository"]["html_url"],
-    repoName: pullReq["repository"]["name"],
-    repoId: pullReq["repository"]["id"],
-  };
+  // const pullData = {
+  //   pull_id: pullReq["pull_request"]["id"],
+  //   number: pullReq["number"],
+  //   title: pullReq["pull_request"]["title"],
+  //   body: pullReq["pull_request"]["body"],
+  //   action: pullReq["action"],
+  //   pull_html_url: pullReq["pull_request"]["html_url"],
+  //   user_name: pullReq["user"]["login"],
+  //   user_id: pullReq["user.id"],
+  //   created: pullReq["created_at"],
+  //   closed: pullReq["closed_at"],
+  //   avatarUserUrl: pullReq["user"]["avatar_url"],
+  //   user_html_url: pullReq["user.url"],
+  //   repo_html_url: pullReq["repository"]["html_url"],
+  //   repoName: pullReq["repository"]["name"],
+  //   repoId: pullReq["repository"]["id"],
+  // };
   try {
     set(
       ref(
@@ -37,7 +37,21 @@ const addPullReqToDB = async (pullReq) => {
         `repo/${pullReq.repository.id}/pull_requests/${pullReq.pull_request.id}`
       ),
       {
-        pullData,
+        pull_id: pullReq["pull_request"]["id"],
+        number: pullReq["number"],
+        title: pullReq["pull_request"]["title"],
+        body: pullReq["pull_request"]["body"],
+        action: pullReq["action"],
+        pull_html_url: pullReq["pull_request"]["html_url"],
+        user_name: pullReq["user"]["login"],
+        user_id: pullReq["user.id"],
+        created: pullReq["created_at"],
+        closed: pullReq["closed_at"],
+        avatarUserUrl: pullReq["user"]["avatar_url"],
+        user_html_url: pullReq["user.url"],
+        repo_html_url: pullReq["repository"]["html_url"],
+        repoName: pullReq["repository"]["name"],
+        repoId: pullReq["repository"]["id"],
       }
     );
     return true;
