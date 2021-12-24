f0198308f3d770daf2921b2bcaebec5577a82243
YahavMizrahi <yahav283@gmail.com>
2021-12-24 09:16:03 +0200

    firebase

--- src/controllers/notifications/pull-request.js
+++ src/controllers/notifications/pull-request.js
@@ -12,15 +12,32 @@ const printData = async (req, res) => {
   res.send("hi", ...data.data);
 };
 const addPullReqToDB = async (pullReq) => {
-  try {
-    set(ref(database, `repo/${pullReq.repository.id}`), pullReq);
+    const pullData = {
+      pull_id: pullReq.pull_request.id,
+      number: pullReq.number,
+      title: pullReq.pull_request.title,
+      body: pullReq.pull_request.body,
+      action: pullReq.action,
+      pull_html_url: pullReq.pull_request.html_url,
+      user_name: pullReq.user.login,
+      user_id: pullReq.user.id,
+      created: pullReq.created_at,
+      closed: pullReq.closed_at,
+      avatarUserUrl: pullReq.user.avatar_url,
+      user_html_url: pullReq.user.url,
+      repo_html_url: pullReq.repository.html_url,
+      repoName: pullReq.repository.name,
+      repoId: pullReq.repository.id,
+    };
+    try {
+      set(ref(database, `repo/${pullReq.repository.id}`), pullData);
 
-    // database.ref.child(`repo/${pullReq.repository.id}`).set(pullReq);
-    return true;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
+      // database.ref.child(`repo/${pullReq.repository.id}`).set(pullReq);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
 };
 
 const pullRequest = (req, res, next) => {
