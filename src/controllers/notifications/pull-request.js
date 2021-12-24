ff45f1b3d0f7dddbb7f2bd8b606d8b823738bb35
YahavMizrahi <yahav283@gmail.com>
2021-12-24 09:57:51 +0200

    firebase

--- src/controllers/notifications/pull-request.js
+++ src/controllers/notifications/pull-request.js
@@ -13,28 +13,11 @@ const printData = async (req, res) => {
 };
 
 const addPullReqToDB = async (pullReq) => {
-  // const pullData = {
-  //   pull_id: pullReq["pull_request"]["id"],
-  //   number: pullReq["number"],
-  //   title: pullReq["pull_request"]["title"],
-  //   body: pullReq["pull_request"]["body"],
-  //   action: pullReq["action"],
-  //   pull_html_url: pullReq["pull_request"]["html_url"],
-  //   user_name: pullReq["user"]["login"],
-  //   user_id: pullReq["user.id"],
-  //   created: pullReq["created_at"],
-  //   closed: pullReq["closed_at"],
-  //   avatarUserUrl: pullReq["user"]["avatar_url"],
-  //   user_html_url: pullReq["user.url"],
-  //   repo_html_url: pullReq["repository"]["html_url"],
-  //   repoName: pullReq["repository"]["name"],
-  //   repoId: pullReq["repository"]["id"],
-  // };
   try {
     set(
       ref(
         database,
-        `repo/${pullReq.repository.id}/pull_requests/${pullReq.pull_request.id}`
+        `repo/${pullReq["repository"]["id"]}/pull_requests/${pullReq["pull_request"]["id"]}`
       ),
       {
         pull_id: pullReq["pull_request"]["id"],
